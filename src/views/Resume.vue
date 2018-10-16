7351bf0c8a813b90c7e6d82038a9b98c5e7330fb
boyzhaotian <13426031783@139.com>
2018-10-17 03:28:08 +0800

    iframe滚动

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,9 +1,15 @@
 <template>
-    <iframe class="resumeFrame" src="https://boyzhaotian.github.io/resume/" frameborder="0" scrolling="yes"></iframe>
+    <div class="resume">
+        <iframe class="resumeFrame" src="https://boyzhaotian.github.io/resume/" frameborder="0" scrolling="yes"></iframe>
+    </div>
 </template>
 <style scoped>
+.resume {
+    height: 100vh;
+    overflow: auto;
+}
 .resumeFrame {
     width: 100%;
-    height: 100vh;
+    height: 100%;
 }
 </style>
